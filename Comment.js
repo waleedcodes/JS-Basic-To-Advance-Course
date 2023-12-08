828d770388ace6da9b286142d77df18050731111
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2023-12-08 18:21:28 +0500

    some changes

---
+++ Comment.js
@@ -0,0 +1,20 @@
+//  let a = "waleed"; Ctrl + ?
+// let a = "waleed"; use this (//) 
+
+// let a = "waleed";
+// let a = "waleed";
+// let a = "waleed";  Ctrl + ?
+
+
+/* let a = "waleed";
+let a = "waleed";
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; */
