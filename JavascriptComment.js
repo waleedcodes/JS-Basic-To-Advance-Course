bce98763e7ff06a88c0d5e131dcaaa0df5297445
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-01-01 12:27:04 +0500

    added all files

---
+++ JavascriptComment.js
@@ -0,0 +1,34 @@
+//  let a = "waleed"; Ctrl + ?
+// let a = "waleed"; use this (//) 
+
+// let a = "waleed";
+// let a = "waleed";
+// let a = "waleed";  Ctrl + ?
+
+
+/* let a = "waleed";
+let a = "waleed";
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; 
+let a = "waleed"; */
+
+// document.getElementById("demo").innerHTML = "Hello, world";
+// document.write("waleedcodes");
+// console.log("waleedcodes");
+
+
+
+// function clickFunction() {
+//     // document.getElementById("demo").innerHTML = "Hello, world";
+//     // window.alert("Hello, world");
+//     console.log("clickFunction")
+// }
+
+{/* <button onclick="clickFunction()">click</button> */}
---
+++ JavascriptOutputs.js
@@ -0,0 +1,8 @@
+// JavaScript Output
+// JavaScript Display Possibilities
+// (InnerHtml) Done!
+// document.write() Done!
+// window.alert()
+// console.log()
+
+// document.getElementById("demo").innerHTML = "Hello world "
