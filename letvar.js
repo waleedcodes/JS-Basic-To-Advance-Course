98bbcd367f13680fe51247a7644973de082ecb20
WaleedCodes <110061477+waleedcodes@users.noreply.github.com>
2024-01-13 19:57:28 +0500

    let var and const variables

---
+++ letvar.js
@@ -0,0 +1,17 @@
+// var waleed = "waleed ishfaq";
+// let waleed = "waleed ishfaq";
+// console.log(waleed)
+
+let admin = "admin";
+
+admin = 0;
+admin = true;
+
+console.log(admin)
+
+const hello = () => {
+    let waleed = "waleed codes";
+
+}
+
+hello();
